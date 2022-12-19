87bb14c439c34b4f30fa46d6c9f9cef42a76a1ad
endlessmomo <soohyuk96@naver.com>
2022-12-19 11:50:57 +0900

    chore : 최초 환경 설정

---
+++ src/main/java/yuki/account/BeAccountSystemApplication.java
@@ -0,0 +1,13 @@
+package yuki.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAccountSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeAccountSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yuki/account/BeAccountSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package yuki.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAccountSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
