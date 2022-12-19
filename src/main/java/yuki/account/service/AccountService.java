c07d7e74bbc01ec80e18b90216c1ea2053979952
endlessmomo <soohyuk96@naver.com>
2022-12-19 14:16:20 +0900

    feat(AccountService) : 계좌 생성 로직 구현

--- src/main/java/yuki/account/service/AccountService.java
+++ src/main/java/yuki/account/service/AccountService.java
@@ -2,29 +2,56 @@ package yuki.account.service;
 
 import yuki.account.domain.Account;
 import yuki.account.domain.AccountStatus;
+import yuki.account.domain.AccountUser;
+import yuki.account.dto.AccountDto;
 import yuki.account.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import yuki.account.repository.AccountUserRepository;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static yuki.account.domain.AccountStatus.IN_USE;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
     private final AccountRepository accountRepository;
+    private final AccountUserRepository accountUserRepository;
 
+    /**
+     * @param userId
+     * @param basicBalance 사용자가 있는지 확인지 조회
+     *                     계좌의 번호를 생성
+     *                     계좌를 레파지토리의 저장하고, 그 정보를 반환한다.
+     */
     @Transactional
-    public void createAccount() {
-        Account account = Account.builder()
-                .accountNumber("40000")
-                .accountStatus(AccountStatus.IN_USE)
-                .build();
-        accountRepository.save(account);
+    public AccountDto createAccount(Long userId, Long basicBalance) {
+        AccountUser accountUser = accountUserRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자가 존재하지 않습니다."));
+
+        String newAccountNumber = accountRepository.findFirstByOrderByIdDesc()
+                .map(account -> (Integer.parseInt(account.getAccountNumber()) + 1) + "")
+                .orElse("1000000000");
+
+        return AccountDto.fromEntity(
+                accountRepository.save(
+                        Account.builder()
+                                .accountUser(accountUser)
+                                .accountStatus(IN_USE)
+                                .accountNumber(newAccountNumber)
+                                .balance(basicBalance)
+                                .registeredAt(LocalDateTime.now())
+                                .build()
+                )
+        );
     }
 
     @Transactional
     public Account getAccount(Long id) {
-        if(id < 0){
+        if (id < 0) {
             throw new RuntimeException("Minus");
         }
         return accountRepository.findById(id).get();
