ae2e9bc4351ac7f34e4dfc3ed76e30acc176dfb1
endlessmomo <soohyuk96@naver.com>
2022-12-19 13:00:00 +0900

    feat(JpaAuditingConfiguration) : Jpa 사용을 위한 AuditingConfiguration 생성

---
+++ src/main/java/yuki/account/domain/AccountUser.java
@@ -0,0 +1,18 @@
+package yuki.account.domain;
+
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class AccountUser extends BaseEntity{
+    private String name;
+}
