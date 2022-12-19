e743dadebd64ff6a128a08252cdcd049ee377107
endlessmomo <soohyuk96@naver.com>
2022-12-19 12:55:52 +0900

    feat(CreateAccount) : 각 멤버 변수 Validate 어노테이션 작성

--- src/main/java/yuki/account/dto/CreateAccount.java
+++ src/main/java/yuki/account/dto/CreateAccount.java
@@ -3,6 +3,8 @@ package yuki.account.dto;
 import lombok.*;
 import yuki.account.domain.BaseEntity;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 public class CreateAccount {
@@ -10,8 +12,13 @@ public class CreateAccount {
     @Getter
     @Setter
     public static class Request {
+        @NotNull
+        @Min(1)
         private Long userId;
-        private Long balance;
+
+        @NotNull
+        @Min(100)
+        private Long basicBalance;
     }
 
     @Getter
