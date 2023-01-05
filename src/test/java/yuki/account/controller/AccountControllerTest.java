9b31f49886cb39125634c74e3b67858ef883e309
endlessmomo <soohyuk96@naver.com>
2023-01-05 20:48:42 +0900

    chore(AccountControllerTest) : 불필요한 import 삭제

--- src/test/java/yuki/account/controller/AccountControllerTest.java
+++ src/test/java/yuki/account/controller/AccountControllerTest.java
@@ -1,6 +1,5 @@
 package yuki.account.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
