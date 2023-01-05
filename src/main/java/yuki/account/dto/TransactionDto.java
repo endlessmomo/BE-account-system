5fb99109794aca30e436fe9990b7db1fb89d50f3
endlessmomo <soohyuk96@naver.com>
2023-01-05 19:18:48 +0900

    feat(TransactionDto) : TransactionDto로 반환해주는 fromEntity 메소드 구현

---
+++ src/main/java/yuki/account/dto/TransactionDto.java
@@ -0,0 +1,35 @@
+package yuki.account.dto;
+
+import lombok.*;
+import yuki.account.Type.TransactionResultType;
+import yuki.account.Type.TransactionType;
+import yuki.account.domain.Transaction;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TransactionDto {
+    private String accountNumber;
+    private TransactionType transactionType;
+    private TransactionResultType transactionResultType;
+    private Long amount;
+    private Long balanceSnapshot;
+    private String transactionId;
+    private LocalDateTime transactedAt;
+
+    public static TransactionDto fromEntity(Transaction transaction){
+        return TransactionDto.builder()
+                .accountNumber(transaction.getAccount().getAccountNumber())
+                .transactionType(transaction.getTransactionType())
+                .transactionResultType(transaction.getTransactionResultType())
+                .amount(transaction.getAmount())
+                .balanceSnapshot(transaction.getBalanceSnapshot())
+                .transactionId(transaction.getTransactionId())
+                .transactedAt(transaction.getTransactedAt())
+                .build();
+    }
+}
