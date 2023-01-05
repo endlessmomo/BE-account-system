629e638e3bf40a8df716a042c9396d74c123f5b4
endlessmomo <soohyuk96@naver.com>
2023-01-05 19:10:55 +0900

    feat(ErrorCode) : ErrorCode 추가 에러 Update

--- src/main/java/yuki/account/Type/ErrorCode.java
+++ src/main/java/yuki/account/Type/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
     , UN_MATCH_USER_ACCOUNT("사용자와 계좌 소유주가 다릅니다.")
     , ACCOUNT_BALANCE_NOT_EMPTY("계좌에 잔액이 남아있는 경우 해지할 수 없습니다.")
     , ALREADY_ACCOUNT_UNREGISTERED("계좌가 이미 해지된 상태입니다.")
+    , EXCEED_BALANCE("계좌에 잔액을 초과하는 금액은 결제할 수 없습니다.")
     ;
 
     private final String description;
