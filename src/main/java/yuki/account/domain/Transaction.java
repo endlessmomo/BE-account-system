b22efd9719bb45cf2a7b823df3560b6098d98d8e
endlessmomo <soohyuk96@naver.com>
2023-01-09 16:03:15 +0900

    feat(Transaction) : 중복 취소 방지를 위한 변수 추가

--- src/main/java/yuki/account/domain/Transaction.java
+++ src/main/java/yuki/account/domain/Transaction.java
@@ -3,10 +3,13 @@ package yuki.account.domain;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import yuki.account.Type.TransactionResultType;
+import yuki.account.Type.TransactionStatus;
 import yuki.account.Type.TransactionType;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+
+import static yuki.account.Type.TransactionStatus.CANCELED;
 
 @Getter
 @Setter
@@ -20,6 +23,8 @@ public class Transaction extends BaseEntity {
     private TransactionType transactionType;
     @Enumerated(EnumType.STRING)
     private TransactionResultType transactionResultType;
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus transactionStatus;
 
     @ManyToOne
     private Account account;
@@ -28,4 +33,8 @@ public class Transaction extends BaseEntity {
 
     private String transactionId;
     private LocalDateTime transactedAt;
+
+    public void canceledBalance(TransactionStatus status){
+        this.transactionStatus = status;
+    }
 }
