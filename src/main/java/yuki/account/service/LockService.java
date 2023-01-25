1c2bdd89124e18e9fe83e36c23d0902752bb3b46
endlessmomo <soohyuk96@naver.com>
2023-01-25 23:23:59 +0900

    feat(LockService) : Redis를 통한 트랜잭션에 Lock을 걸기 위한 서비스 로직 구현

--- src/main/java/yuki/account/service/LockService.java
+++ src/main/java/yuki/account/service/LockService.java
@@ -20,18 +20,20 @@ public class LockService {
         RLock lock = redissonClient.getLock(getLockKey(accountNumber));
         log.debug("Trying lock for accountNumber : {}", accountNumber);
 
-        try{
-            boolean isLock = lock.tryLock(1, 5, TimeUnit.SECONDS);
-            if(!isLock){
+        try {
+            boolean isLock = lock.tryLock(1, 15, TimeUnit.SECONDS);
+            if (!isLock) {
                 log.error("====== Lock acquisition failed ======");
                 throw new AccountException(ErrorCode.ACCOUNT_TRANSACTION_LOCK);
             }
+        } catch (AccountException e) {
+            throw e;
         } catch (Exception e) {
-            log.error("Redis lock failed");
+            log.error("Redis lock failed", e);
         }
     }
 
-    public void unLock(String accountNumber){
+    public void unLock(String accountNumber) {
         log.debug("Unlock for accountNumber : {}", accountNumber);
         redissonClient.getLock(getLockKey(accountNumber)).unlock();
     }
