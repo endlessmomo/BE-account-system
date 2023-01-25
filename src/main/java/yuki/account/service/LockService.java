4c9b67556e67bdfb888dc0a692e299135a392e1b
endlessmomo <soohyuk96@naver.com>
2023-01-25 22:46:39 +0900

    feat(LockService) : Lock을 위한 서비스 로직 구현

---
+++ src/main/java/yuki/account/service/LockService.java
@@ -0,0 +1,42 @@
+package yuki.account.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Service;
+import yuki.account.Type.ErrorCode;
+import yuki.account.exception.AccountException;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class LockService {
+    private final RedissonClient redissonClient;
+
+    public void lock(String accountNumber) {
+        RLock lock = redissonClient.getLock(getLockKey(accountNumber));
+        log.debug("Trying lock for accountNumber : {}", accountNumber);
+
+        try{
+            boolean isLock = lock.tryLock(1, 5, TimeUnit.SECONDS);
+            if(!isLock){
+                log.error("====== Lock acquisition failed ======");
+                throw new AccountException(ErrorCode.ACCOUNT_TRANSACTION_LOCK);
+            }
+        } catch (Exception e) {
+            log.error("Redis lock failed");
+        }
+    }
+
+    public void unLock(String accountNumber){
+        log.debug("Unlock for accountNumber : {}", accountNumber);
+        redissonClient.getLock(getLockKey(accountNumber)).unlock();
+    }
+
+    private static String getLockKey(String accountNumber) {
+        return "ACLK:" + accountNumber;
+    }
+}
